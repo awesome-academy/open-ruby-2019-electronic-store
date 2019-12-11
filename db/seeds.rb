44d960eb736bc2c59591736843ce12ae59064e01
DangSy <dangsy1998@gamil.com>
2019-12-11 23:19:28 +0700

    Show product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+  name = FFaker::Bank.name
+  detail = FFaker::Lorem.sentences(3)
+  keyword = FFaker::Lorem.sentences(1)
+  Category.create!(name: name, detail: detail, keyword: keyword)
+end
+
+30.times do
+  name = FFaker::Company.name
+  content = FFaker::Lorem.sentences(3)
+  address = FFaker::Address
+  Brand.create!(name: name, content: content, address: address)
+end
+
+categories = Category.order(:created_at).take(6)
+brands = Brand.order(:created_at).take(6)
+30.times do |n|
+  name = FFaker::Product.name
+  decription = FFaker::Lorem.sentences(5)
+  quantity = FFaker::SSN
+  if n != 0 
+    Product.create!(name: name, decription: decription, quantity: quantity, brand_id: n, category_id: n) 
+  end
+end
