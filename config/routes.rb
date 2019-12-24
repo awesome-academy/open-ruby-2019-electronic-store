14a0210d3727caef57d1c617cb4bceba4ba3db12
DangSy <dangsy1998@gamil.com>
2019-12-24 11:49:06 +0700

    detail product

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resources :users
-  resources :products, only: %i(index)
+  resources :products, only: %i(index show)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 30.times do
   name = FFaker::Bank.name
-  detail = FFaker::Lorem.sentences(3)
+  detail = FFaker::Lorem.sentences(2)
   keyword = FFaker::Lorem.sentences(1)
   Category.create!(name: name, detail: detail, keyword: keyword)
 end
 
 30.times do
   name = FFaker::Company.name
-  content = FFaker::Lorem.sentences(3)
+  content = FFaker::Lorem.sentences(2)
   address = FFaker::Address
   Brand.create!(name: name, content: content, address: address)
 end
@@ -16,7 +16,7 @@ categories = Category.order(:created_at).take(6)
 brands = Brand.order(:created_at).take(6)
 30.times do |n|
   name = FFaker::Product.name
-  decription = FFaker::Lorem.sentences(5)
+  decription = FFaker::Lorem.sentences(2)
   quantity = FFaker::SSN
   if n != 0 
     Product.create!(name: name, decription: decription, quantity: quantity, brand_id: n, category_id: n) 
